739ddaf587f32156cb3ae2445d06ba16255aca02
Kent <itskent@protonmail.com>
2022-03-06 18:01:37 -0800

    Initial Commit

---
+++ parstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  parstagram
+//
+//  Created by kentmacmini on 3/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
